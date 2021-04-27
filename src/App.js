72a641f5fbfea749d053e7ce7a64abddaff1f403
shallihan <smhallihan@gmail.com>
2021-04-27 12:40:36 +0100

    adding Card component to App with CSS styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import Header from './features/Header/Header';
+import Subreddits from './features/Subreddits/Subreddits';
+import Home from './features/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
             <Header />
+            <main>
+              <Home />
+            </main>
+            <aside>
+              <Subreddits />
+            </aside>
     </div>
   );
 }
