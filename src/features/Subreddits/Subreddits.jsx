ebaf3ac9e7f65ecd6abee221f9ddd9e3b6b6593a
shallihan <smhallihan@gmail.com>
2021-04-27 12:43:13 +0100

    adding Subreddits to App with initial css styles

--- src/features/Subreddits/Subreddits.jsx
+++ src/features/Subreddits/Subreddits.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './Subreddits.css';
+import Card from '../../components/Card/Card';
 
 const Subreddits = () => {
-
+ return (
+     <Card className="subreddit-card">
+         <h2>Subreddits</h2>
+         <ul className="subreddits-list">
+             <lil>4chan</lil>
+             <li>sadcringe</li>
+             <li>nonono</li>
+             <li>science</li>
+             <li>MemeEconony</li>
+             <li>futurology</li>
+         </ul>
+    </Card>
+ );
 }
 
 export default Subreddits;
