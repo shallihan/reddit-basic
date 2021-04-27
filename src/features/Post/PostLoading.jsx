2543591e285c7a6b9adf961841cc428313758dce
shallihan <smhallihan@gmail.com>
2021-04-27 10:41:25 +0100

    initialised react components, changed index.js to a react file

---
+++ src/features/Post/PostLoading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './PostLoading.css';
+
+const PostLoading = () => {
+
+}
+
+export default PostLoading;
---
+++ src/features/Subreddits/Subreddits.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Subreddits.css';
+
+const Subreddits = () => {
+
+}
+
+export default Subreddits;
