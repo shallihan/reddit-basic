71013d9a4ed2e39ffb2637524e0aa3c747cdd2bb
shallihan <smhallihan@gmail.com>
2021-04-27 10:59:59 +0100

    added skeleton header to App.js

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { FaReddit } from 'react-icons/fa';
+import { HiSearch } from 'react-icons/hi';
 import './Header.css';
 
 const Header = () => {
+    return (
+        <header>
+            <div>
+                <FaReddit />
+                <p>Reddit<span>basic</span></p>
+            </div>
+            <form>
+                <input type="text" placeholder="Search" />
+                <button type="submit"><HiSearch /></button>
+            </form>
+        </header>
+    );
 
 }
 
